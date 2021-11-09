daef556da972689d2c83ac4b139469ba8fdc6dca
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-11-09 12:26:59 +0100

    File formatting + repo setup

---
+++ stlLexer.py
@@ -0,0 +1,145 @@
+# Generated from stl.g4 by ANTLR 4.9.2
+from antlr4 import *  # pylint: disable=unused-wildcard-import
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+	from typing import TextIO
+else:
+	from typing.io import TextIO  # type: ignore
+
+
+def serializedATN():
+	with StringIO() as buf:
+		buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2!")
+		buf.write("\u0091\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+		buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+		buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+		buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+		buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+		buf.write("\t\36\4\37\t\37\4 \t \3\2\3\2\3\3\3\3\3\4\3\4\3\5\3\5")
+		buf.write("\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3\n\3\n\3\13\3\13\3\f")
+		buf.write("\3\f\3\r\3\r\3\16\3\16\3\17\3\17\3\17\3\20\3\20\3\21\3")
+		buf.write("\21\3\21\3\22\3\22\3\23\3\23\3\23\3\24\3\24\3\25\3\25")
+		buf.write("\3\26\3\26\3\27\3\27\3\30\3\30\3\31\3\31\3\32\3\32\3\33")
+		buf.write("\3\33\3\33\3\34\3\34\3\34\3\35\6\35~\n\35\r\35\16\35\177")
+		buf.write("\3\36\6\36\u0083\n\36\r\36\16\36\u0084\3\36\3\36\3\37")
+		buf.write("\3\37\7\37\u008b\n\37\f\37\16\37\u008e\13\37\3 \3 \2\2")
+		buf.write("!\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f\27\r\31")
+		buf.write("\16\33\17\35\20\37\21!\22#\23%\24\'\25)\26+\27-\30/\31")
+		buf.write("\61\32\63\33\65\34\67\359\36;\37= ?!\3\2\t\5\2((``\u2229")
+		buf.write("\u2229\4\2XX\u222a\u222a\4\2//\u00ae\u00ae\3\2\62;\5\2")
+		buf.write("\13\f\17\17\"\"\5\2C\\aac|\5\2\62;C\\c|\2\u0093\2\3\3")
+		buf.write("\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2")
+		buf.write("\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2")
+		buf.write("\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2")
+		buf.write("\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\2#\3\2\2\2\2%\3\2")
+		buf.write("\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2\2-\3\2\2\2\2/\3")
+		buf.write("\2\2\2\2\61\3\2\2\2\2\63\3\2\2\2\2\65\3\2\2\2\2\67\3\2")
+		buf.write("\2\2\29\3\2\2\2\2;\3\2\2\2\2=\3\2\2\2\2?\3\2\2\2\3A\3")
+		buf.write("\2\2\2\5C\3\2\2\2\7E\3\2\2\2\tG\3\2\2\2\13I\3\2\2\2\r")
+		buf.write("K\3\2\2\2\17M\3\2\2\2\21O\3\2\2\2\23Q\3\2\2\2\25S\3\2")
+		buf.write("\2\2\27U\3\2\2\2\31W\3\2\2\2\33Y\3\2\2\2\35[\3\2\2\2\37")
+		buf.write("^\3\2\2\2!`\3\2\2\2#c\3\2\2\2%e\3\2\2\2\'h\3\2\2\2)j\3")
+		buf.write("\2\2\2+l\3\2\2\2-n\3\2\2\2/p\3\2\2\2\61r\3\2\2\2\63t\3")
+		buf.write("\2\2\2\65v\3\2\2\2\67y\3\2\2\29}\3\2\2\2;\u0082\3\2\2")
+		buf.write("\2=\u0088\3\2\2\2?\u008f\3\2\2\2AB\7*\2\2B\4\3\2\2\2C")
+		buf.write("D\7+\2\2D\6\3\2\2\2EF\7}\2\2F\b\3\2\2\2GH\7\177\2\2H\n")
+		buf.write("\3\2\2\2IJ\7]\2\2J\f\3\2\2\2KL\7_\2\2L\16\3\2\2\2MN\7")
+		buf.write("~\2\2N\20\3\2\2\2OP\7/\2\2P\22\3\2\2\2QR\7-\2\2R\24\3")
+		buf.write("\2\2\2ST\7,\2\2T\26\3\2\2\2UV\7\61\2\2V\30\3\2\2\2WX\7")
+		buf.write("\60\2\2X\32\3\2\2\2YZ\7@\2\2Z\34\3\2\2\2[\\\7@\2\2\\]")
+		buf.write("\7?\2\2]\36\3\2\2\2^_\7>\2\2_ \3\2\2\2`a\7>\2\2ab\7?\2")
+		buf.write("\2b\"\3\2\2\2cd\7?\2\2d$\3\2\2\2ef\7#\2\2fg\7?\2\2g&\3")
+		buf.write("\2\2\2hi\7\u25cc\2\2i(\3\2\2\2jk\7\u25a3\2\2k*\3\2\2\2")
+		buf.write("lm\7W\2\2m,\3\2\2\2no\t\2\2\2o.\3\2\2\2pq\t\3\2\2q\60")
+		buf.write("\3\2\2\2rs\t\4\2\2s\62\3\2\2\2tu\7\u2194\2\2u\64\3\2\2")
+		buf.write("\2vw\7/\2\2wx\7@\2\2x\66\3\2\2\2yz\7?\2\2z{\7@\2\2{8\3")
+		buf.write("\2\2\2|~\t\5\2\2}|\3\2\2\2~\177\3\2\2\2\177}\3\2\2\2\177")
+		buf.write("\u0080\3\2\2\2\u0080:\3\2\2\2\u0081\u0083\t\6\2\2\u0082")
+		buf.write("\u0081\3\2\2\2\u0083\u0084\3\2\2\2\u0084\u0082\3\2\2\2")
+		buf.write("\u0084\u0085\3\2\2\2\u0085\u0086\3\2\2\2\u0086\u0087\b")
+		buf.write("\36\2\2\u0087<\3\2\2\2\u0088\u008c\t\7\2\2\u0089\u008b")
+		buf.write("\t\b\2\2\u008a\u0089\3\2\2\2\u008b\u008e\3\2\2\2\u008c")
+		buf.write("\u008a\3\2\2\2\u008c\u008d\3\2\2\2\u008d>\3\2\2\2\u008e")
+		buf.write("\u008c\3\2\2\2\u008f\u0090\7.\2\2\u0090@\3\2\2\2\6\2\177")
+		buf.write("\u0084\u008c\3\b\2\2")
+		return buf.getvalue()
+
+
+class stlLexer(Lexer):
+
+	atn = ATNDeserializer().deserialize(serializedATN())
+
+	decisionsToDFA = [DFA(ds, i) for i, ds in enumerate(atn.decisionToState)]
+
+	OPEN_BRACKET = 1
+	CLOSE_BRACKET = 2
+	OPEN_CURLY = 3
+	CLOSE_CURLY = 4
+	OPEN_SQUARE = 5
+	CLOSE_SQUARE = 6
+	PIPELINE = 7
+	MINUS = 8
+	PLUS = 9
+	STAR = 10
+	FORWARD_SLASH = 11
+	DOT = 12
+	LARGER_THAN = 13
+	LARGER_THAN_OR_EQUAL = 14
+	SMALLER_THAN = 15
+	SMALLER_THAN_OR_EQUAL = 16
+	EQUALS = 17
+	UNEQUALS = 18
+	DIAMOND = 19
+	SQUARE = 20
+	UNTIL = 21
+	AND = 22
+	OR = 23
+	NEGATE = 24
+	ARROW = 25
+	SIGNLE_ARROW = 26
+	DOUBLE_ARROW = 27
+	DIGIT = 28
+	WHITE_SPACE = 29
+	SIGNAL = 30
+	COMMA = 31
+
+	channelNames = [u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN"]
+
+	modeNames = ["DEFAULT_MODE"]
+
+	literalNames = [
+	    "<INVALID>", "'('", "')'", "'{'", "'}'", "'['", "']'", "'|'", "'-'",
+	    "'+'", "'*'", "'/'", "'.'", "'>'", "'>='", "'<'", "'<='", "'='", "'!='",
+	    "'\u25CA'", "'\u25A1'", "'U'", "'\u2192'", "'->'", "'=>'", "','"
+	]
+
+	symbolicNames = [
+	    "<INVALID>", "OPEN_BRACKET", "CLOSE_BRACKET", "OPEN_CURLY",
+	    "CLOSE_CURLY", "OPEN_SQUARE", "CLOSE_SQUARE", "PIPELINE", "MINUS",
+	    "PLUS", "STAR", "FORWARD_SLASH", "DOT", "LARGER_THAN",
+	    "LARGER_THAN_OR_EQUAL", "SMALLER_THAN", "SMALLER_THAN_OR_EQUAL",
+	    "EQUALS", "UNEQUALS", "DIAMOND", "SQUARE", "UNTIL", "AND", "OR",
+	    "NEGATE", "ARROW", "SIGNLE_ARROW", "DOUBLE_ARROW", "DIGIT",
+	    "WHITE_SPACE", "SIGNAL", "COMMA"
+	]
+
+	ruleNames = [
+	    "OPEN_BRACKET", "CLOSE_BRACKET", "OPEN_CURLY", "CLOSE_CURLY",
+	    "OPEN_SQUARE", "CLOSE_SQUARE", "PIPELINE", "MINUS", "PLUS", "STAR",
+	    "FORWARD_SLASH", "DOT", "LARGER_THAN", "LARGER_THAN_OR_EQUAL",
+	    "SMALLER_THAN", "SMALLER_THAN_OR_EQUAL", "EQUALS", "UNEQUALS", "DIAMOND",
+	    "SQUARE", "UNTIL", "AND", "OR", "NEGATE", "ARROW", "SIGNLE_ARROW",
+	    "DOUBLE_ARROW", "DIGIT", "WHITE_SPACE", "SIGNAL", "COMMA"
+	]
+
+	grammarFileName = "stl.g4"
+
+	def __init__(self, input=None, output: TextIO = sys.stdout):
+		super().__init__(input, output)
+		self.checkVersion("4.9.2")
+		self._interp = LexerATNSimulator(
+		    self, self.atn, self.decisionsToDFA, PredictionContextCache()
+		)
+		self._actions = None
+		self._predicates = None
