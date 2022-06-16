cbdfdcc8a49253d3b17c2c9258b2f1b94a7d5897
Pieter Hendriks <hendriks.p.pieter@gmail.com>
2022-06-16 15:28:44 +0200

    Improved chdir in test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """ Main """
 # pylint: disable-all
 import os
-os.chdir('stlTool')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import unittest
 if __name__ == "__main__":
 	from tests import *
